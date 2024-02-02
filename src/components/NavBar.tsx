2494137a11ce998b4e920392be40913d11849954
unknown <patiladi3119@gmail.com>
2024-02-02 11:24:29 +0530

    Added dark scheme color mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/unsplash_icon.svg";
+import { HStack, Image, Text, useColorMode } from "@chakra-ui/react";
+import logo_black from "../assets/unsplash_icon.svg";
+import logo_white from "../assets/unsplash_icon_white.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
+  const { colorMode } = useColorMode();
+  const logoSrc = colorMode === "dark" ? logo_white : logo_black;
+
   return (
-    <HStack padding="10px">
-      <Image src={logo} boxSize="50px" alt="Unsplash Logo" />
-      <Text>NavBar</Text>
+    <HStack padding="10px" justifyContent="space-between">
+      <Image src={logoSrc} boxSize="50px" alt="Unsplash Logo" />
+      <ColorModeSwitch />
     </HStack>
   );
 };
